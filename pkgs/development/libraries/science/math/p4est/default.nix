8964683f794361cc2b08eba3e482ce325f83c9fa
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2021-09-20 23:00:39 +0200

    p4est: propagate metis build input

--- pkgs/development/libraries/science/math/p4est/default.nix
+++ pkgs/development/libraries/science/math/p4est/default.nix
@@ -22,8 +22,8 @@ stdenv.mkDerivation {
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
-  propagatedBuildInputs = [ p4est-sc ];
-  buildInputs = lib.optional withMetis metis;
+  propagatedBuildInputs = [ p4est-sc ]
+    ++ lib.optional withMetis metis;
   inherit debugEnable mpiSupport withMetis;
 
   preConfigure = ''
